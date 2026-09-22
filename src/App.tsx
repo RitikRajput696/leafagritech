fccacf2fa355041b91ad5b0ce04cd2a476d4e433
ritikrajput696 <rixusinput696@gmail.com>
2026-09-22 23:26:40 +0530

    added gallery version

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import Services from "./pages/Services";
 import Projects from "./pages/Projects";
+import Gallery from "./pages/Gallery";
 
 export default function App() {
   return (
@@ -15,6 +16,7 @@ export default function App() {
         <Route path="/Projects" element={<Projects />} />
         <Route path="/Services" element={<Services />} />
         <Route path="/About" element={<About />} />
+        <Route path="/Gallery" element={<Gallery />} />
       </Routes>
     </>
   );
